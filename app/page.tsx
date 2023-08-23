57a91c8694847418d3372a829da1c09bddc1e5e2
Vivek Rekhadia <vivekrekhadia007@gmail.com>
2023-08-23 18:07:36 +0530

    error changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "auto";
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
